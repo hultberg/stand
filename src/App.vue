fff874e190a900f793ffd065a7434dec5f915f89
Edvin Hultberg <hultberg@mittnett.net>
2017-11-25 21:35:15 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="main">
+    <h2>Stand up in</h2>
+    <timer
+      @timer-start="handleTimerStart"
+      @timer-done="handleTimerDone"
+      :max-time="20"
+      :set-title="true">
+    </timer>
+    <div v-if="isTimerDone"><button @click="$refs.timer.restartTimer">Restart</button></div>
+  </div>
+</template>
+
+<script lang="coffee">
+import Timer from './Timer.vue'
+
+export default
+  name: 'app'
+
+  components:
+    timer: Timer
+
+  data: ->
+    {
+      isTimerDone: null
+    }
+
+  methods:
+    handleTimerStart: -> @isTimerDone = false
+
+    handleTimerDone: ->
+      window.alert("It's time to stand up")
+      @isTimerDone = true
+</script>
+
+<style lang="sass">
+  html, body, .main
+    height: 100%
+
+  html
+    font-size: 100%
+    box-sizing: border-box
+
+  *, *::after, *::before
+    box-sizing: inherit
+
+  body
+    background-color: #343434
+    color: #cfcfcf
+    font-family: Arial, Helvetica, sans-serif
+    font-size: 24px
+
+  .main
+    width: 100%
+    margin-left: auto
+    margin-right: auto
+    display: flex
+    flex-direction: column
+    justify-content: center
+    align-items: center
+
+    h2
+      font-size: 5vw
+      margin-bottom: 1rem
+
+    .time
+      font-size: 2vw
+      display: flex
+</style>
---
+++ src/Timer.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="timer">{{ timeLeftString }}</div>
+</template>
+
+<script lang="coffee">
+export default
+  name: 'timer'
+
+  props:
+    # Unit: minutes
+    maxTime:
+      default: 1
+      type: Number
+
+    setTitle:
+      default: true
+      type: Boolean
+
+  data: ->
+    {
+      # Unit: seconds
+      timeLeft: 0
+
+      currentTimerInterval: null
+      documentUnwatch: null
+    }
+
+  computed:
+    maxTimeSeconds: -> @maxTime * 60
+    isFinished: -> @timeLeft < 1
+    timeLeftString: ->
+      mins = Math.floor((@timeLeft / 60) % 60)
+      secs = Math.floor(@timeLeft % 60)
+
+      return "#{(if mins > 0 then "#{mins}m " else '')}#{(if secs > 0 then "#{secs}s" else '')}"
+
+  created: ->
+    # If prop allows it, we can modify the document title.
+    @initDocumentTitleWatcher() if @setTitle
+
+    # Finally, init and start the timer.
+    @restartTimer()
+
+  beforeDestroy: ->
+    @stopTimer()
+    @documentUnwatch() if @documentUnwatch?
+
+  methods:
+    initDocumentTitleWatcher: ->
+      @documentUnwatch = @$watch 'timeLeft', (value) ->
+        suffix = 'standup'
+
+        if value > 0 then document.title = "#{@timeLeftString} until #{suffix}"
+        if @isFinished then document.title = "!!! Time to #{suffix} !!!"
+
+    restartTimer: ->
+      @initTimer()
+      @startTimer()
+
+    initTimer: -> @timeLeft = @maxTimeSeconds
+
+    startTimer: ->
+      console.log 'starting timer'
+      @$emit 'timer-start'
+      console.log 'emitted event?'
+      @stopTimer()
+      @currentTimerInterval = setInterval(@handleStopwatch.bind(@), (1 * 1000))
+
+    stopTimer: ->
+      clearInterval @currentTimerInterval if @currentTimerInterval > 0
+
+    handleStopwatch: ->
+      @timeLeft -= 1
+
+      if @timeLeft < 1
+        @$emit 'timer-done'
+        @stopTimer()
+
+</script>
+
+<style lang="sass">
+</style>
